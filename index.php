e5cd6399d6aecf3e57c9efcd016eff8d42a0e762
Cut0x <71967731+Cut0x@users.noreply.github.com>
2024-12-15 11:38:42 +0100

    patch

--- index.php
+++ index.php
@@ -27,7 +27,6 @@ if ($page) {
             $fileResponse = file_get_contents($file['download_url'], false, $context);
             $fileContent = $parsedown->text($fileResponse);
 
-            // Extract author and date from the file content
             if (preg_match('/^Author:\s*(.+)$/m', $fileResponse, $matches)) {
                 $fileAuthor = $matches[1];
             }
@@ -63,39 +62,18 @@ if (empty($fileContent)) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap" rel="stylesheet">
-    <style>
-        #scrollToTopBtn {
-            display: none;
-            position: fixed;
-            bottom: 20px;
-            right: 30px;
-            z-index: 99;
-            font-size: 18px;
-            border: none;
-            outline: none;
-            background-color: #00d4ff;
-            color: white;
-            cursor: pointer;
-            padding: 15px;
-            border-radius: 10px;
-        }
-
-        #scrollToTopBtn:hover {
-            background-color: #35b6cf;
-        }
-    </style>
 </head>
 <body style="background-color:  #1e1e30;">
     <header class="navbar navbar-expand-lg py-3">
         <div class="container">
-            <a class="navbar-brand" href="https://docs.akirasteam.com/" style="color: white;">Docs.</a>
+            <a class="navbar-brand" href="<?php echo $config['site']['url']; ?>" style="color: white;">Docs.</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon" style="color: white;"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link" href="https://akirasteam.com/#about">About</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?php echo $config['site']['update']; ?>">Blog</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#ABOUT">About</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#BLOG">Blog</a></li>
                     <li class="nav-item"><a class="nav-link" href="mailto:<?php echo $config['site']['contact_email']; ?>">Contact</a></li>
                 </ul>
             </div>
@@ -123,7 +101,7 @@ if (empty($fileContent)) {
     </div>
     <footer class="footer">
         <div class="footer-content">
-            <p>&copy; 2023 AkirasTeam. All rights reserved.</p>
+            <p>&copy; <?php echo date('Y'); ?> <?php echo $config['site']['name']; ?>. All rights reserved.</p>
             <p>Follow us on <a href="<?php echo $config['links']['twitter']; ?>">Twitter</a>, <a href="<?php echo $config['links']['instagram']; ?>">Instagram</a>, and <a href="<?php echo $config['links']['github']; ?>">Github</a>.</p>
         </div>
     </footer>
@@ -132,10 +110,8 @@ if (empty($fileContent)) {
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
     <script>
-        // Get the button
         var mybutton = document.getElementById("scrollToTopBtn");
 
-        // When the user scrolls down 20px from the top of the document, show the button
         window.onscroll = function() {scrollFunction()};
 
         function scrollFunction() {
@@ -146,7 +122,6 @@ if (empty($fileContent)) {
             }
         }
 
-        // When the user clicks on the button, scroll to the top of the document
         function topFunction() {
             document.body.scrollTop = 0;
             document.documentElement.scrollTop = 0;
